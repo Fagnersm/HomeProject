fb76272e12700577f0db9b85a48c73c3ca55f31f
¨Renan <renan.oliveira@dce.ufpb.br>
2011-09-03 18:07:19 -0300

    Adicionado anotacões JPA e métodos sets e gets

--- src/java/br/com/sistcomp/model/Imovel.java
+++ src/java/br/com/sistcomp/model/Imovel.java
@@ -7,12 +7,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 /**
  * Classe Imovel representa a entidade Imovel.
  *
  * @author Jefferson Araújo, jefferssonaraujo@gmail.com
+ * @author Renan Oliveira, renan.oliveira@dce.ufpb.br
  * @version 1.0
  * @since 28/08/2011
  *
@@ -27,10 +30,37 @@ public class Imovel implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "imo_imoveis_id")
     private Integer id;
+    @Column(name = "imo_imoveis_descricaoImovel", length = 50, nullable = false,
+        unique=false)
     private String descricaoImovel;
+    @Column(name = "imo_imoveis_endereco", length = 50, nullable = false,
+        unique=false)
+    @OneToOne
     private Endereco endereco;
+    @Column(name = "imo_imoveis_observacaoSobreImovel", length = 50,
+        nullable = false, unique=false)
     private String observacaoSobreImovel;
+    @Column(name = "imo_imoveis_fotosImoveis", length = 50, nullable = false,
+        unique=false)
+    @OneToMany
     private List<Foto> fotosImoveis;
+
+    //Construtores
+
+    public Imovel(){
+
+    }
+
+    public Imovel(String descricaoImovel, Endereco endereco,
+            String observacaoSobreImovel, List<Foto> fotosImoveis) {
+
+        this.descricaoImovel = descricaoImovel;
+        this.endereco = endereco;
+        this.observacaoSobreImovel = observacaoSobreImovel;
+        this.fotosImoveis = fotosImoveis;
+    }
+
+
 
     //getter and setter
     public Integer getId() {
@@ -40,6 +70,40 @@ public class Imovel implements Serializable {
     public void setId(Integer id) {
         this.id = id;
     }
+
+    public String getDescricaoImovel() {
+        return descricaoImovel;
+    }
+
+    public void setDescricaoImovel(String descricaoImovel) {
+        this.descricaoImovel = descricaoImovel;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public List<Foto> getFotosImoveis() {
+        return fotosImoveis;
+    }
+
+    public void setFotosImoveis(List<Foto> fotosImoveis) {
+        this.fotosImoveis = fotosImoveis;
+    }
+
+    public String getObservacaoSobreImovel() {
+        return observacaoSobreImovel;
+    }
+
+    public void setObservacaoSobreImovel(String observacaoSobreImovel) {
+        this.observacaoSobreImovel = observacaoSobreImovel;
+    }
+
+
 
     @Override
     public int hashCode() {
@@ -55,7 +119,8 @@ public class Imovel implements Serializable {
             return false;
         }
         Imovel other = (Imovel) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+        if ((this.id == null && other.id != null) || (this.id != null &&
+                !this.id.equals(other.id))) {
             return false;
         }
         return true;
