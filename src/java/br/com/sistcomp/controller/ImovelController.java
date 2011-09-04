f733926616325e39f31e3f6eaa9837e3be5c4f27
¨Renan <renan.oliveira@dce.ufpb.br>
2011-09-04 20:32:17 -0300

    Classe do pacote controller, mas ainda não foi toda implementada.

---
+++ src/java/br/com/sistcomp/controller/ImovelController.java
@@ -0,0 +1,25 @@
+package br.com.sistcomp.controller;
+
+import br.com.sistcomp.dao.ImovelDao;
+import javax.annotation.ManagedBean;
+import javax.ejb.EJB;
+
+/**
+ * Classe ImovelController.
+ *
+ * @author Renan Oliveira, renan.oliveira@dce.ufpb.br
+ * @version 1.0
+ * @since 31/08/2011
+ *
+ * Copyright (C) 2011 SistComp Empresa Jr.
+ */
+@ManagedBean(value = "imovelController")
+public class ImovelController {
+
+    @EJB
+    ImovelDao imovelDao;
+
+    //TODO implementar a classe controller
+
+
+}
