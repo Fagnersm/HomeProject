e438febaf094afc12ead12723566c0d8e543f6dd
Fagner Silva <fagner.silva@sistcomp.com.br>
2011-09-08 14:45:51 -0300

    classe de negocio onde ira tratar a entidade Bairro

---
+++ src/java/br/com/sistcomp/dao/BairroDaoImpl.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sistcomp.dao;
+
+import br.com.sistcomp.model.Bairro;
+import java.util.List;
+
+/**
+ *
+ * @author fagner
+ */
+public class BairroDaoImpl implements BairroDao{
+
+    @Override
+    public void salvarBairro(Bairro bairro) {
+        
+    }
+
+    @Override
+    public void removerBairro(Bairro bairro) {
+        
+    }
+
+    @Override
+    public Bairro buscarBairro(Bairro bairro) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<Bairro> listarBairros() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
